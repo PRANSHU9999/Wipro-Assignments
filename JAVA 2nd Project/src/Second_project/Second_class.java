ebf8f86bd8c193324c3d6db3720630d57bc71f3c
PRANSHU9999 <mishrapranshu727@gmail.com>
2026-04-20 16:46:20 +0530

    2nd push

---
+++ JAVA 2nd Project/src/Second_project/Second_class.java
@@ -0,0 +1,10 @@
+package Second_project;
+
+public class Second_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("2nd try");
+	}
+
+}
